a60f2f449a941cd8bb6f6fc9f506967ca1f988be
Bianca Teixeira <135990504+biancadpteixeiraa@users.noreply.github.com>
2024-03-12 15:28:30 -0300

    Update home.php

--- src/view/home.php
+++ src/view/home.php
@@ -3,26 +3,27 @@ require_once 'C:/xampp/htdocs/projeto/valida_acesso.php';
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
     <meta charset="utf-8">
     <title>Opções do Aluno</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="text-center mb-4">
-                    <h2>Opções do Usuário</h2>
+                    <h2 class="text-primary">Área do Aluno</h2>
                 </div>
                 <div class="text-center">
                     <a href="cadastrar_aluno.php" class="btn btn-primary btn-lg mr-4">Cadastrar</a>
                     <a href="listar_alunos.php" class="btn btn-secondary btn-lg">Ver Lista de Cadastros</a>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 </body>
 </html>
