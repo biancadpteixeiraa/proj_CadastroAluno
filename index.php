0c9ca3119b10c4961803f1cd6e1e80a8916f9b88
Bianca Teixeira <135990504+biancadpteixeiraa@users.noreply.github.com>
2024-03-12 15:29:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,58 @@
-<?php
+<?php 
 
 ?>
-
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Cadastro de Alunos</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-        <style>
-            .card-login {
-            padding: 50px;
-            width: 400px;
-            margin: 0 auto;
-            margin-top: 50px;
-            }
-        </style>
-    </head>
-    <body>
-        <nav class="navbar navbar-dark bg-dark">
-            <a href="#" class="navbar-brand">
-                Cadastro Alunos
-            </a>
-        </nav>
-        <div class="container">
-            <div class="row">
-                <div class="card-login">
-                    <div class="card">
-                        <div class="card-header">
-                            Login
-                        </div>
-                        <div class="card-body">
-                            <form action="valida_login.php" method="post">
-                                <div class="form-group">
-                                    <input name="email" type="email" class="form-control" placeholder="E-mail">
-                                </div>
-                                <div class="form-group">
-                                    <input name="senha" type="password" class="form-control" placeholder="Senha">
-                                </div>
-                                <?php if (isset($_GET['login']) && $_GET['login'] == 'erro') {?>
-                                <div class="text-danger">
-                                    Usuário ou senha inválido(s)
-                                </div>
-                                    <?php } ?>
-                                <button class="btn btn-lg btn-info btn-block" type="submit">
-                                    Entrar
-                                </button>
-                            </form>
-                        </div>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Alunos</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #F8F7FF;
+        }
+        .card {
+            border: none;
+            border-radius: 20px;
+            box-shadow: 0 20px 6px rgba(0,0,0,.1);
+        }
+        .card-header {
+            background-color: #007bff;
+            color: white;
+            border-radius: 10px 10px 0 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="col-md-8">Login - Área do Aluno</h5>
+                    </div>
+                    <div class="card-body">
+                        <form action="valida_login.php" method="post">
+                            <div class="form-group">
+                                <input name="email" type="email" class="form-control" placeholder="E-mail" required>
+                            </div>
+                            <div class="form-group">
+                                <input name="senha" type="password" class="form-control" placeholder="Senha" required>
+                            </div>
+                            <?php if (isset($_GET['login']) && $_GET['login'] == 'erro') {?>
+                            <div class="alert alert-danger" role="alert">
+                                Usuário ou senha inválido(s)
+                            </div>
+                            <?php } ?>
+                            <button class="btn btn-primary btn-block" type="submit">
+                                Entrar
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </body>
-    
+    </div>
+</body>
 </html>
